b9f5633c6a5b2a01f12f6da46937d256a705a930
8954sood <8954sood@naver.com>
2025-01-23 02:04:30 +0900

    feat: Add Pretendard Font

--- lib/designsystem/foundation/app_typography.dart
+++ lib/designsystem/foundation/app_typography.dart
@@ -2,178 +2,211 @@ import 'package:flutter/material.dart';
 
 abstract class AppTypography {
   static const display1Bold = TextStyle(
+    fontFamily: "pretendard",
     fontSize: 36,
     fontWeight: FontWeight.bold,
-    height: 1.3
+    height: 1.3,
   );
   static const display1Medium = TextStyle(
-      fontSize: 36,
-      fontWeight: FontWeight.w500,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 36,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
   );
   static const display1Regular = TextStyle(
-      fontSize: 36,
-      fontWeight: FontWeight.w400,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 36,
+    fontWeight: FontWeight.w400,
+    height: 1.3,
   );
 
   static const display2Bold = TextStyle(
-      fontSize: 32,
-      fontWeight: FontWeight.bold,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 32,
+    fontWeight: FontWeight.bold,
+    height: 1.3,
   );
   static const display2Medium = TextStyle(
-      fontSize: 32,
-      fontWeight: FontWeight.w500,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 32,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
   );
   static const display2Regular = TextStyle(
-      fontSize: 32,
-      fontWeight: FontWeight.w400,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 32,
+    fontWeight: FontWeight.w400,
+    height: 1.3,
   );
 
   static const title1Bold = TextStyle(
-      fontSize: 28,
-      fontWeight: FontWeight.bold,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 28,
+    fontWeight: FontWeight.bold,
+    height: 1.3,
   );
   static const title1Medium = TextStyle(
-      fontSize: 28,
-      fontWeight: FontWeight.w500,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 28,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
   );
   static const title1Regular = TextStyle(
-      fontSize: 28,
-      fontWeight: FontWeight.w400,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 28,
+    fontWeight: FontWeight.w400,
+    height: 1.3,
   );
 
   static const title2Bold = TextStyle(
-      fontSize: 26,
-      fontWeight: FontWeight.bold,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 26,
+    fontWeight: FontWeight.bold,
+    height: 1.3,
   );
   static const title2Medium = TextStyle(
-      fontSize: 26,
-      fontWeight: FontWeight.w500,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 26,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
   );
   static const title2Regular = TextStyle(
-      fontSize: 26,
-      fontWeight: FontWeight.w400,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 26,
+    fontWeight: FontWeight.w400,
+    height: 1.3,
   );
 
   static const title3Bold = TextStyle(
-      fontSize: 24,
-      fontWeight: FontWeight.bold,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 24,
+    fontWeight: FontWeight.bold,
+    height: 1.3,
   );
   static const title3Medium = TextStyle(
-      fontSize: 24,
-      fontWeight: FontWeight.w500,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 24,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
   );
   static const title3Regular = TextStyle(
-      fontSize: 24,
-      fontWeight: FontWeight.w400,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 24,
+    fontWeight: FontWeight.w400,
+    height: 1.3,
   );
 
   static const heading1Bold = TextStyle(
-      fontSize: 22,
-      fontWeight: FontWeight.bold,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 22,
+    fontWeight: FontWeight.bold,
+    height: 1.3,
   );
   static const heading1Medium = TextStyle(
-      fontSize: 22,
-      fontWeight: FontWeight.w500,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 22,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
   );
   static const heading1Regular = TextStyle(
-      fontSize: 22,
-      fontWeight: FontWeight.w400,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 22,
+    fontWeight: FontWeight.w400,
+    height: 1.3,
   );
 
   static const heading2Bold = TextStyle(
-      fontSize: 20,
-      fontWeight: FontWeight.bold,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 20,
+    fontWeight: FontWeight.bold,
+    height: 1.3,
   );
   static const heading2Medium = TextStyle(
-      fontSize: 20,
-      fontWeight: FontWeight.w500,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 20,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
   );
   static const heading2Regular = TextStyle(
-      fontSize: 20,
-      fontWeight: FontWeight.w400,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 20,
+    fontWeight: FontWeight.w400,
+    height: 1.3,
   );
 
   static const headlineBold = TextStyle(
-      fontSize: 18,
-      fontWeight: FontWeight.bold,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 18,
+    fontWeight: FontWeight.bold,
+    height: 1.3,
   );
   static const headlineMedium = TextStyle(
-      fontSize: 18,
-      fontWeight: FontWeight.w500,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 18,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
   );
   static const headlineRegular = TextStyle(
-      fontSize: 18,
-      fontWeight: FontWeight.w400,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 18,
+    fontWeight: FontWeight.w400,
+    height: 1.3,
   );
 
   static const bodyBold = TextStyle(
-      fontSize: 16,
-      fontWeight: FontWeight.bold,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 16,
+    fontWeight: FontWeight.bold,
+    height: 1.3,
   );
   static const bodyMedium = TextStyle(
-      fontSize: 16,
-      fontWeight: FontWeight.w500,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
   );
   static const bodyRegular = TextStyle(
-      fontSize: 16,
-      fontWeight: FontWeight.w400,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 16,
+    fontWeight: FontWeight.w400,
+    height: 1.3,
   );
 
   static const labelBold = TextStyle(
-      fontSize: 14,
-      fontWeight: FontWeight.bold,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 14,
+    fontWeight: FontWeight.bold,
+    height: 1.3,
   );
   static const labelMedium = TextStyle(
-      fontSize: 14,
-      fontWeight: FontWeight.w500,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 14,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
   );
   static const labelRegular = TextStyle(
-      fontSize: 14,
-      fontWeight: FontWeight.w400,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    height: 1.3,
   );
 
   static const captionBold = TextStyle(
-      fontSize: 12,
-      fontWeight: FontWeight.bold,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 12,
+    fontWeight: FontWeight.bold,
+    height: 1.3,
   );
   static const captionMedium = TextStyle(
-      fontSize: 12,
-      fontWeight: FontWeight.w500,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 12,
+    fontWeight: FontWeight.w500,
+    height: 1.3,
   );
   static const captionRegular = TextStyle(
-      fontSize: 12,
-      fontWeight: FontWeight.w400,
-      height: 1.3
+    fontFamily: "pretendard",
+    fontSize: 12,
+    fontWeight: FontWeight.w400,
+    height: 1.3,
   );
 }
